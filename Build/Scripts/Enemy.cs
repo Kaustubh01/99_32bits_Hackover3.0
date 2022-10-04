cad190c82de4e4704c621c809389c01ffd9ae67d
Kaustubh <kaustubhmayekar02@gmail.com>
2022-10-05 03:14:00 +0530

    New Theme

---
+++ Build/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed
+{
+	//Enemy inherits from MovingObject, our base class for objects that can move, Player also inherits from this.
+	public class Enemy : MovingObject
+	{
+		public int playerDamage; 							//The amount of food points to subtract from the player when attacking.
+		public AudioClip attackSound1;						//First of two audio clips to play when attacking the player.
+		public AudioClip attackSound2;						//Second of two audio clips to play when attacking the player.
+		
+		
+		private Animator animator;							//Variable of type Animator to store a reference to the enemy's Animator component.
+		private Transform target;							//Transform to attempt to move toward each turn.
+		private bool skipMove;								//Boolean to determine whether or not enemy should skip a turn or move this turn.
+		protected override void Start ()
+		{GameManager.instance.AddEnemyToList (this);
+			animator = GetComponent<Animator> ();
+			target = GameObject.FindGameObjectWithTag ("Player").transform;
+			base.Start ();
+		}
+		protected override void AttemptMove <T> (int xDir, int yDir)
+		{
+			if(skipMove)
+			{
+				skipMove = false;
+				return;
+				
+			}
+			base.AttemptMove <T> (xDir, yDir);
+			skipMove = true;
+		}
+		public void MoveEnemy ()
+		{
+			int xDir = 0;
+			int yDir = 0;
+			if(Mathf.Abs (target.position.x - transform.position.x) < float.Epsilon)
+				yDir = target.position.y > transform.position.y ? 1 : -1;
+			else
+				xDir = target.position.x > transform.position.x ? 1 : -1;
+			AttemptMove <Player> (xDir, yDir);
+		}
+		protected override void OnCantMove <T> (T component)
+		{
+			Player hitPlayer = component as Player;
+			
+			hitPlayer.LoseFood (playerDamage);
+			animator.SetTrigger ("enemyAttack");
+			SoundManager.instance.RandomizeSfx (attackSound1, attackSound2);
+		}
+	}
+}
